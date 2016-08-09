378ee664ae228143dfbc67c1ec01a6be155aebb7
kll098 <kelly@MacBook-Pro-van-Kelly.local>
2016-08-09 20:11:14 +0200

    NIEUWE VERSIE
    9 augustus 2016

--- js/custom.js
+++ js/custom.js
@@ -1,104 +1,145 @@
-/*
-Author URI: http://webthemez.com/
-Note: 
-Licence under Creative Commons Attribution 3.0 
-Do not remove the back-link in this web template 
--------------------------------------------------------*/
-
-$(window).load(function() {
-    jQuery('#all').click();
-    return false;
-});
-
-$(document).ready(function() {
-	$('.carousel').carousel();
-    $('#header_wrapper').scrollToFixed();
-    $('.res-nav_click').click(function() {
-        $('.main-nav').slideToggle();
-        return false
-
-    });
-	
-    var container = $('#portfolio_wrapper');
+(function ($) {
+	"use strict";
 
 
-    container.isotope({
-        animationEngine: 'best-available',
-        animationOptions: {
-            duration: 200,
-            queue: false
-        },
-        layoutMode: 'fitRows'
-    });
+	$(document).ready(function() {
 
-    $('#filters a').click(function() {
-        $('#filters a').removeClass('active');
-        $(this).addClass('active');
-        var selector = $(this).attr('data-filter');
-        container.isotope({
-            filter: selector
-        });
-        setProjects();
-        return false;
-    });
+		// jQuery DemoPanel
+		$("#panel_open").click(function(){
+			$(".setting_panel").animate({left:"-226px"});
+			$(this).hide();
+			$("#panel_close").css("display","block");
+		});	
 
-    function splitColumns() {
-        var winWidth = $(window).width(),
-            columnNumb = 1;
+		$("#panel_close").click(function(){
+			$(".setting_panel").animate({left:"0px"});
+			$(this).hide();
+			$("#panel_open").css("display","block");
+		});	
+
+		// jQuery Custom scrollbar
+		$("body").niceScroll({
+			cursorcolor: "#57BDDB",
+			cursorborderradius: "0px",
+			cursorwidth: "10px",
+			cursorminheight: 100,
+			cursorborder: "0px solid #fff",
+			zindex: 9999,
+			autohidemode: true,
+			horizrailenabled:false
+		});
+
+		// jQuery Stick menu		
+		$(".navbar").sticky({topSpacing: 0});
+		
+		// jQuery One page nav
+		$(".nav").onePageNav();
+		
+		// jQuery Portfolio filter
+		$("#portfolio_list").isotope({
+		  // options
+		  itemSelector: '.pitem',
+		  layoutMode: 'fitRows'
+		});	
+		
+		
+		// jQuery Owl Carousel
+		$(".partner-list").owlCarousel({
+			pagination : false,
+			navigation : true,
+			navigationText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"]
+		});
+		
+		
+		// jQuery counter
+		$('.counter').counterUp({
+			delay: 10,
+			time: 1000
+		});
+		
+		// jQuery Lightbox
+		$(".litebox").liteBox();	
+		
+		// jQuery Parallax
+		$('.home-parallax, .features-bg, .parallax-bg').scrolly({bgParallax: true});
+		
+		
+		
+		// jQuery Contact form validation
+		$('form').validatr({
+			location:"bottom"
+		}); 
+		
+		$("#form-messages").hover(function() {
+			$(this).delay(350).fadeOut('slow');
+		});
+		
+		
+
+		
+	});
 
 
-        if (winWidth > 1024) {
-            columnNumb = 4;
-        } else if (winWidth > 900) {
-            columnNumb = 2;
-        } else if (winWidth > 479) {
-            columnNumb = 2;
-        } else if (winWidth < 479) {
-            columnNumb = 1;
-        }
-
-        return columnNumb;
-    }
-	
-    function setColumns() {
-        var winWidth = $(window).width(),
-            columnNumb = splitColumns(),
-            postWidth = Math.floor(winWidth / columnNumb);
-
-        container.find('.portfolio-item').each(function() {
-            $(this).css({
-                width: postWidth + 'px'
-            });
-        });
-    }
-
-    function setProjects() {
-        setColumns();
-        container.isotope('reLayout');
-    }
-
-    container.imagesLoaded(function() {
-        setColumns();
-    });
 
 
-    $(window).bind('resize', function() {
-        setProjects();
-    });
 
-   $(".fancybox").fancybox();
-});
+	/* ==============================================
+	 Smooth Scroll To Anchor
+	 =============================================== */
 
-wow = new WOW({
-    animateClass: 'animated',
-    offset: 100
-});
-wow.init();
-document.getElementById('').onclick = function() {
-    var section = document.createElement('section');
-    section.className = 'wow fadeInDown';
-    section.className = 'wow shake';
-    section.className = 'wow zoomIn';
-    section.className = 'wow lightSpeedIn';
-    this.parentNode.insertBefore(section, this);
-};
+	//jQuery for page scrolling feature - requires jQuery Easing plugin
+	$(function() {
+		$('.navbar a,.btn,.to-top').bind('click', function(event) {
+			var $anchor = $(this);
+			$('html').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top - 50
+			}, 1500, 'easeInOutExpo');
+			event.preventDefault();
+		});
+	});
+
+
+
+
+	$(window).load(function(){
+
+		
+		
+		// Preloader & Option Panel
+		$('.spinner').fadeOut();
+		$('.setting_panel').show();
+		
+		$('#preloader').delay(350).fadeOut('slow');
+		
+		$('body').delay(350);
+		
+
+		// Portfolio Items
+		var $container = $('#portfolio_list');
+		$container.isotope({
+			filter: '*',
+			animationOptions: {
+				duration: 750,
+				easing: 'linear',
+				queue: false
+			}
+		});
+	 
+		$('.portfolio-filters li').click(function(){
+			$('.portfolio-filters .active').removeClass('active');
+			$(this).addClass('active');
+	 
+			var selector = $(this).attr('data-filter');
+			$container.isotope({
+				filter: selector,
+				animationOptions: {
+					duration: 750,
+					easing: 'linear',
+					queue: false
+				}
+			 });
+			 return false;
+		});
+	});
+
+}(jQuery));	
